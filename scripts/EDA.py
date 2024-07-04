8becf1ce83e25936a9e0bfb3f9ff0df807ef469d
Jokoisme <valenciakweman@gmail.com>
2024-07-04 16:01:25 +1000

    Update EDA.py

--- scripts/EDA.py
+++ scripts/EDA.py
@@ -8,9 +8,38 @@ def perform_eda(input_path):
     #data = sns.load_dataset("penguins")
     data = pd.read_csv(input_path)
 
+    # Get species per gender count 
+    sex_column = data['sex']
+    data.groupby(["species", sex_column]).size().unstack(level=1).plot(kind='bar')
+    plt.xlabel("Species") 
+    plt.ylabel("Number of penguins") 
+    plt.title("Number of penguins for each species by gender") 
+    plt.legend() 
+    plt.show() 
+
+    plt.savefig("results/figures/speciesCount.png")
+
     # Basic statistics
     print(data.describe())
 
+    # Set Seaborn style
+    sns.set_style("darkgrid")
+
+    # Identify numerical columns
+    numerical_columns = data.select_dtypes(include=["int64", "float64"]).columns
+
+    # Plot distribution of each numerical feature
+    plt.figure(figsize=(14, len(numerical_columns) * 3))
+    for idx, feature in enumerate(numerical_columns, 1):
+        plt.subplot(len(numerical_columns), 2, idx)
+        sns.histplot(data[feature], kde=True)
+        plt.title(f"{feature} | Skewness: {round(data[feature].skew(), 2)}")
+
+    # Adjust layout and show plots
+    plt.tight_layout()
+    plt.show()
+    plt.savefig("results/figures/numericalPlot.png") 
+    
     # Correlation
     
     # Change String data values to categorical 
@@ -21,7 +50,7 @@ def perform_eda(input_path):
     dataplot = sns.heatmap(data.corr(), cmap="YlGnBu", annot=True) 
     plt.savefig("results/figures/correlation.png") 
     
-    print("EDA completed. Correlation plot saved to results/figures.correlation.png")
+    print("EDA completed. All plot saved to results/figure")
 
 if __name__ == "__main__":
     perform_eda('data/processed/penguins_processed.csv')
